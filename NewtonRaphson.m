1db1391053a44e4f488d09100d4ab7c26741c2dc
felipescordeiro <felipe10souza@yahoo.com.br>
2016-08-29 14:51:11 -0300

    add newtonRaphson

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -1,4 +1,4 @@
-function raiz = NewtonRaphson(func, Xo, erroTolerance)
+function raiz = NewtonRaphson( Xo, erroTolerance)
     %Método NewtonRapshon (aberto)
     %retorna a raiz estimada da função
     %func = função a ser calculada
@@ -11,21 +11,28 @@ function raiz = NewtonRaphson(func, Xo, erroTolerance)
     %|| MOSTRAR ERRO DE ESTIMAÇÃO || MOSTRAR ERRO VERDADEIRO || Verificar
     %função
     
+    function f = fun(x)
+            f=x^3 - 6 * x^2 + 11 * x - 6.1;
+    end
+    function d = der(x)
+            d = 3*x^2 - 12 * x + 11;
+    end
 
-    x = Xo;
-    error = 100;
+    erro_abs = 100;
     iterador = 0;
     
-    df = diff(func);
-    
-    while error > erroTolerance
-        xold = x; %salvando valor antigo
-        fn = func(xold); %calculando valor de f(Xn)
-        phi = df(xold); %calculando valor de f'(Xn)
-        x = abs(xold - fn/phi); %calculando valor de Xn+1
+    while (erro_abs > erroTolerance | iterador < 2)
         iterador = iterador + 1; %iterador de controle
-        
-        error = abs((x-xold)/x); %calculando erro relativo
+        disp ("iterador: "), disp (iterador)
+        xold = Xo; %salvando valor antigo
+        fx = fun(Xo); %calculando valor de f(Xn)
+        fd = der(Xo); %calculando valor de f'(Xn)
+        Xo = abs(xold - (fx/fd)); %calculando valor de Xn+1
+        disp('Xo'), disp(Xo); 
+        disp('fx'), disp(fx); 
+        disp('fd'), disp(fd);
+        erro_abs = abs((Xo-xold)/Xo)*100; %calculando erro relativo
+        disp ("erro_abs:"), disp (erro_abs);
     end
-    raiz = x;
+    raiz = Xo;
 end
