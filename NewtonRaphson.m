dfa33e264554df372e29b6e4f713420627d8a297
lucas <lucas.ecomp2012@gmail.com>
2016-08-29 00:45:30 -0300

    Comentários
    Adicionando comentários e Help da função

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -1,4 +1,17 @@
 function raiz = NewtonRaphson(func, Xo, erroTolerance)
+    %Método NewtonRapshon (aberto)
+    %retorna a raiz estimada da função
+    %func = função a ser calculada
+    %Xo = estimativa inicial
+    %erroTolerance = tolerancia de erro/criterio de parada
+    
+    %Autor: Lucas Morais e Felipe Cordeiro
+    
+    %O QUE FALTA: PLOTAR NO GRÁFICO || MOSTRAR RAIZ ESTIMADA POR ITERAÇÃO
+    %|| MOSTRAR ERRO DE ESTIMAÇÃO || MOSTRAR ERRO VERDADEIRO || Verificar
+    %função
+    
+
     x = Xo;
     error = 100;
     iterador = 0;
@@ -6,13 +19,13 @@ function raiz = NewtonRaphson(func, Xo, erroTolerance)
     df = diff(func);
     
     while error > erroTolerance
-        xold = x; 
-        fn = func(xold);
-        phi = df(xold);
-        x = abs(xold - fn/phi);
-        iterador = iterador + 1;
+        xold = x; %salvando valor antigo
+        fn = func(xold); %calculando valor de f(Xn)
+        phi = df(xold); %calculando valor de f'(Xn)
+        x = abs(xold - fn/phi); %calculando valor de Xn+1
+        iterador = iterador + 1; %iterador de controle
         
-        error = abs((x-xold)/x);
+        error = abs((x-xold)/x); %calculando erro relativo
     end
     raiz = x;
 end
